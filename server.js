bad72bc4e29be493f58dfa0a92e3e6f8745f92e3
Pratik24816 <prajapatipm16@gmail.com>
2025-01-23 09:37:12 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const mongoose=require("mongoose");
+const Document=require("./Document");
+
+const url = "mongodb+srv://prajapatipm16:3ClpFm4uUsxIKPjM@portfolio.noz1l.mongodb.net/PortfolioDB?retryWrites=true&w=majority&appName=PortFolio";
+mongoose.connect(url)
+  .then(() => console.log(`Connected to MongoDB--->${mongoose.connection.host}`))
+  .catch(err => console.log("Error connecting to MongoDB", err));
+
+
+
+const io=require('socket.io')(3001,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"],
+    }
+});
+const defaultValues="";
+
+io.on("connection",socket=>{
+
+    socket.on("get-document",async documentId=>{
+        const document=await findOrCreateDoc(documentId);
+        socket.join(documentId);
+        socket.emit("load-document",document.data)
+
+        socket.on("send-changes",delta=>{
+            socket.broadcast.to(documentId).emit("receive-changes",delta);
+            console.log(socket.id);
+            console.log(delta);
+            console.log(documentId);
+        });
+
+        socket.on("file-imported",delta=>{
+            socket.broadcast.to(documentId).emit("receive-file", delta);
+        });
+
+        socket.on("save-document",async data=>{
+            await Document.findByIdAndUpdate(documentId,{data});
+        });
+
+    });
+
+});
+
+async function findOrCreateDoc(id){
+    if(id==null)
+        return;
+
+    const document=await Document.findById(id);
+    if(document)
+        return document;
+
+    return await Document.create({_id:id,data:defaultValues});
+}
